bb63785aa58066dc24f502b6986a5059eaa5b28d
UmerShahidApachi <umer@apachisol.com>
2020-06-29 15:25:37 +0500

    issue fixed

--- resources/views/backend/admin/category/category.blade.php
+++ resources/views/backend/admin/category/category.blade.php
@@ -1,163 +1,137 @@
 @extends('backend.admin.layouts.app')
 @section('customSection')
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-        <div class="col-md-4">
-          <div class="card card-chart">
-            <div class="card-header card-header-success">
-              <div class="ct-chart" id="dailySalesChart"></div>
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="table-responsive">
+    {{--    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>--}}
+    {{--    <script type="text/javascript" src="//cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>--}}
+    <div class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <div class="table-title">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h2>Manage <b>Categories</b></h2>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#addCategory" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Category</span></a>
+                                {{--						<a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>--}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="table-responsive">
 
-                  <table class="table table-striped table-hover" id="table_id">
-                <thead>
-                    <tr>
-{{--						<th>--}}
-{{--							<span class="custom-checkbox">--}}
-{{--								<input type="checkbox" id="selectAll">--}}
-{{--								<label for="selectAll"></label>--}}
-{{--							</span>--}}
-{{--						</th>--}}
-                        <th>Name</th>
-                        <th>Icon</th>
-{{--						<th>Address</th>--}}
-{{--                        <th>Phone</th>--}}
-                        <th>Actions</th>
-                    </tr>
-                </thead>
+                            <table class="table table-striped table-hover" id="table_id">
+                                <thead>
+                                <tr>
+                                    {{--						<th>--}}
+                                    {{--							<span class="custom-checkbox">--}}
+                                    {{--								<input type="checkbox" id="selectAll">--}}
+                                    {{--								<label for="selectAll"></label>--}}
+                                    {{--							</span>--}}
+                                    {{--						</th>--}}
+                                    <th>Name</th>
+                                    <th>Icon</th>
+                                    {{--						<th>Address</th>--}}
+                                    {{--                        <th>Phone</th>--}}
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
 
-                <tbody>
-                @if($data)
-                    @foreach($data as $row)
-                    <tr>
-{{--						<td>--}}
-{{--							<span class="custom-checkbox">--}}
-{{--								<input type="checkbox" id="checkbox1" name="options[]" value="1">--}}
-{{--								<label for="checkbox1"></label>--}}
-{{--							</span>--}}
-{{--						</td>--}}
+                                <tbody>
+                                @if($data)
+                                    @foreach($data as $row)
+                                        <tr>
+                                            {{--						<td>--}}
+                                            {{--							<span class="custom-checkbox">--}}
+                                            {{--								<input type="checkbox" id="checkbox1" name="options[]" value="1">--}}
+                                            {{--								<label for="checkbox1"></label>--}}
+                                            {{--							</span>--}}
+                                            {{--						</td>--}}
 
-                        <td>{{$row->name}}</td>
-                            <td>
-                                @if($row->logo!="")
-                                    <img src="{{asset('category/' .$row->logo)}}" style="width: 75px;" >
-                                @else
-                                    <img src="{{url('image/dummy.jpg')}} " style="width: 75px;">
+                                            <td>{{$row->name}}</td>
+                                            <td>
+                                                @if($row->logo!="")
+                                                    <img src="{{asset('category/' .$row->logo)}}" style="width: 75px;" >
+                                                @else
+                                                    <img src="{{url('image/dummy.jpg')}} " style="width: 75px;">
+                                                @endif
+                                            </td>
+
+                                            {{--						<td>89 Chiaroscuro Rd, Portland, USA</td>--}}
+                                            {{--                        <td>(171) 555-2222</td>--}}
+                                            <td>
+                                                <a href="" data-id="{{$row->id}}" id="edit_cat"  class="edit category_edit"><i class="material-icons" data-toggle="" title="Edit">&#xE254;</i></a>
+                                                <a href="#" data-id="{{$row->id}}"  class="delete removePartner" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
                                 @endif
-                            </td>
-
-                            {{--						<td>89 Chiaroscuro Rd, Portland, USA</td>--}}
-{{--                        <td>(171) 555-2222</td>--}}
-                        <td>
-
-                            <a href="" data-id="{{$row->id}}" id="edit_cat"  class="edit category_edit"><i class="material-icons" data-toggle="" title="Edit">&#xE254;</i></a>
-                            <a href="#" data-id="{{$row->id}}"  class="delete removePartner" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                        </td>
-
-                    </tr>
-                    @endforeach
-                    @endif
-                </tbody>
-              </table>
-                  </div>
-              </div>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-
-<div id="edit_category" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form  id="category_form" method="POST" action="{{url('admin/update/category')}}" enctype="multipart/form-data">
-                @csrf
-                <div class="modal-header">
-                    <h4 class="modal-title">Edit Slider</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                </div>
-
-                <div class="modal-body" id="replace1">
-                </div>
-                <div class="tab-pane" id="settings">
-                  <table class="table">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="form-check-input" type="checkbox" value="">
-                              <span class="form-check-sign">
-                                <span class="check"></span>
-                              </span>
-                            </label>
-                          </div>
-                        </td>
-                        <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
-                        <td class="td-actions text-right">
-                          <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                            <i class="material-icons">edit</i>
-                          </button>
-                          <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                            <i class="material-icons">close</i>
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="form-check-input" type="checkbox" value="" checked>
-                              <span class="form-check-sign">
-                                <span class="check"></span>
-                              </span>
-                            </label>
-                          </div>
-                        </td>
-                        <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                        </td>
-                        <td class="td-actions text-right">
-                          <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                            <i class="material-icons">edit</i>
-                          </button>
-                          <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                            <i class="material-icons">close</i>
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="form-check">
-                            <label class="form-check-label">
-                              <input class="form-check-input" type="checkbox" value="" checked>
-                              <span class="form-check-sign">
-                                <span class="check"></span>
-                              </span>
-                            </label>
-                          </div>
-                        </td>
-                        <td>Sign contract for "What are conference organizers afraid of?"</td>
-                        <td class="td-actions text-right">
-                          <button type="button" rel="tooltip" title="Edit Task" class="btn btn-primary btn-link btn-sm">
-                            <i class="material-icons">edit</i>
-                          </button>
-                          <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-link btn-sm">
-                            <i class="material-icons">close</i>
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+    <div id="addCategory" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form  id="category_form" method="POST" action="{{route('save_category')}}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title">Add Category</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="file-field">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" required>
+                            </div>
+                            <div class="btn btn-primary btn-sm float-left">
+                                <span>Choose file</span>
+                                <input type="file" name="logo" accept="image/*" required>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+                        <input type="submit" class="btn btn-success cat" id="cat">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div id="edit_category" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form  id="category_form" method="POST" action="{{url('admin/update/category')}}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title">Edit Slider</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body" id="replace1">
+                    </div>
+
+                    <div class="modal-footer">
+                        <input type="button"  class="btn btn-default" data-dismiss="modal" value="Cancel">
+                        <input type="submit" class="btn btn-info" value="Update">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    {{--@section('script')--}}
+
 @endsection
 <link rel="stylesheet" href="http://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
@@ -207,7 +181,6 @@
 
 </script>
 <script>
-
     $(document).on('click', '.category_edit', function (evt) {
         var task_id = $( this ).attr( "data-id" );
         var form_data = {
@@ -218,7 +191,6 @@
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
-
             url: '{{url('admin/edit/category')}}',
             data: form_data,
             success: function (response) {
@@ -226,7 +198,6 @@
                     var html ="";
                     html += '<input type="hidden" name="id" value="'+response.data.id+ '" >';
                     html += '<div class="form-group">' ;
-
                     html +=  '<label>Name</label>' ;
                     html += '<input type="text" class="form-control" value="'+response.data.name+ '" name="name" required>' ;
                     html +=  '</div>' ;
@@ -242,4 +213,3 @@
         });
     });
 </script>
->>>>>>> 7bcf166d9bff424bb37fc2328be1a1bdf30eb4f3
