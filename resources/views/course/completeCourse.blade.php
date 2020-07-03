02cc8023db2d8af0df759737f0cb4c3370fbc40e
usman khan <UMKHAN93@GMAIL.COM>
2020-07-03 12:04:07 +0500

    commit all

---
+++ resources/views/course/completeCourse.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.main')
+@section('offline-course')
+
+
+
+
+
+  <!--Header End Here  -->
+  <div class="plum-bg pt-3 pb-3">
+    <div class="container row">
+      <div class="col-sm-8">
+        <h3 class="complete_course_classes_heading pull-left">Finance compelet course Classes</h3>
+      </div>
+      <div class="col-sm-4 d-flex">
+        <ul class="ulDBlock-top compelet_course_nav2 pull-right d-flex">
+          <li>
+            <div class="font16 white-text"><img src="img/share_icon.png"></div>
+          </li>
+          <li>
+            <div class="font16 white-text"><img src="img/favourit.png"></div>
+          </li>
+          <li>
+            <div class="font16 white-text"><img src="img/enroll.png"></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- add video iframe -->
+  <div class="row">
+    <div class="col-sm-8 mt-0 p-0">
+      <iframe width="100%" height="400px;" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1">
+      </iframe>
+      <!-- <div class="plum-bg pt-3 pb-3">
+        <div class="container">
+          <ul class="ulDBlock-top img_footer">
+            <li>
+              <div class="font16 white-text"> <img src="img/teacher_activ.png"><span class="teacher_activ_n">Trainer
+                  Name</span></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/Line_img.png"></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/parson-1.png"></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/bac_per_img.png" class="parson_profile">
+                <img src="img/Ellipse.png" class="parson_Ellipse">
+                <img src="img/person_big.png" class="parson_img_big">
+                <span class="parson_name">M.Asif</span>
+                <span class="parson_skills">Accounting trainer</span>
+                <img src="img/ratting_str.png" class="ratting_str"></div>
+            </li>
+            <li>
+              <div class="font16 white-text parson_3"> <img src="img/parson-3.png"></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/parson-4.png"></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/parson-5.png"></div>
+            </li>
+            <li>
+              <div class="font16 white-text"> <img src="img/parson-6.png"></div>
+            </li>
+
+          </ul>
+        </div>
+      </div> -->
+    </div>
+    <div class="col-sm-4 mt-0 p-0">
+      <div class="course_content d-flow-root">
+        <p class="pull-left">Course content</p>
+        <p class="close_icon pull-right">×</p>
+      </div>
+      
+      
+    </div>
+  </div>
+
+  <!--  add page content  -->
+  <div class="content_page mar-100">
+    <div class="flex-center mb-1 mt-1 request_certificate_div">
+      <u><a class="Socialb plum-text" href="#"><img src="img/request_certificate.png"></a></u>
+    </div>
+    <div class="content">
+      <p>Course Overview</p>
+      <p class="flex-center mb-1 mt-1">تختلف مواصفات ومقومات المذيع الواجب توفرها، باختلاف مجال اختصاصه، وهناك فرقاً
+        نوعياً بين المواصفات المطلوبة لدى للتقديم التلفزيوني عن المواصفات الواجب توافرها لدى المذيع الإذاعي، كما أنّ
+        هناك اختلافاً شاسعاً في مواصفات كلٍّ من المذيع السياسي والمذيع الاقتصادي، وبكن هناك بعض المواصفات العامة،
+        والمشتركة في المجالين التلفزيوني والإذاع بهذه الدورة تتمكن من تحقيق عدة أهداف
+      </p>
+      <br>
+      <p>محاور الدورة</p>
+      <br>
+      <p> مدخل في العمل التلفزيوني</p>
+      <br>
+      <p> دور المقدم التلفزيوني</p>
+      <br>
+      <p>صفات المذيع أو المقدم الناجح</p>
+      <br>
+      <p>مهارات التقديم التفزيوني</p>
+      <br>
+      <p>إجادة الحوار والسيطرة على الموضوع</p>
+      <br>
+      <p> كيفية التعامل مع الكاميرا</p>
+      <br>
+      <p> إدارة الحوار التلفزيوني</p>
+      <br>
+      <p>نتائج الدورة</p>
+      <br>
+
+      <p>كتساب مهارات التقديم التلفزيوني</p>
+      <p>امتلاك المعرفة الأساسية في التقديم التلفزيوني.</p>
+      <p>كيفية التعامل مع الكميرا.</p>
+      <p>كيفية إدارة الحوار التلفزيوني.</p>
+      <p>الفئة المستهدفة</p>
+      <br>
+      <p>جميع المهتمين بصناعة المحتوى والتقديم التلفزيوني والتعامل المباشر مع الكميرا ومن لديه شغف بالاعلام وصناعته .
+      </p>
+    </div>
+  </div>
+
+  <!--Description-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
