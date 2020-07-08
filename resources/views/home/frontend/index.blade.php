300bf60e575004fd34d014323b853dbe311bc105
rizwan <rizwanmanzoor200@gmail.com>
2020-07-08 11:31:14 +0500

    landing page changes

--- resources/views/home/frontend/index.blade.php
+++ resources/views/home/frontend/index.blade.php
@@ -315,6 +315,27 @@
   padding-left: 10px;
   color: white;
 }
+.cat-title {
+  font-weight: 600;
+  font-size: 28px;
+  color: #343A40;
+  text-transform: capitalize;
+}
+.student-box {
+  border-radius: 10px 20px !important;
+  box-shadow: 5px 5px 4px 4px rgba(0, 0, 0, 0.102) !important;
+}
+.student-box:hover {
+  overflow: hidden;
+  -webkit-transform: scale(1.02);
+}
+.stu-description {
+  font-size: 13px;
+  font-weight: 400;
+  padding: 0 5px;
+  overflow: hidden;
+  margin-top: 10px;
+}
 
 @media (max-width: 992px) {
   .middlesec-p1 {
@@ -813,7 +834,7 @@
 <!-- old -->
     <div class="container mt-5 mb-5">
         <div class="carousel slide multi-item-carousel" id="theCarousel">
-            <div class="carousel-inner row w-100 ">
+            <div class="carousel-inner row w-100 d-none">
                 <div class="carousel-item active multi-img p-0">
                     <div class="">
                         @if(isset($latest1))
@@ -952,7 +973,13 @@
                 <div class="" style="min-height: 100px ;overflow: hidden;">
                     @foreach($categories1 as $cat)
 
-                    <div class="slider_header text-center m-2"><u>{{$cat->name}}</u></div>
+                    <!-- <div class="slider_header text-center m-2"><u>{{$cat->name}}</u></div> -->
+
+                    <!-- <div class="slider_header text-center m-2 cat-title">
+                      <u style="text-decoration: none !important;">{{$cat->name}}</u>
+                    </div> -->
+                    <!-- <hr style="width: 20%; height: 5px; border-top:2px solid #570055; margin-top: 0" /> -->
+
                     <!--Carousel Wrapper-->
                     <div id="multi-item-{{$cat->name}}" class="carousel slide carousel-multi-item" data-ride="carousel">
 
@@ -975,14 +1002,13 @@
                                     <div class="row">
                                         @foreach($data as $c)
 
-                                        <div class="col-4 col-md-4 p-mob">
+                                        <!-- <div class="col-4 col-md-4 p-mob">
                                             <div class="card">
                                                 <img class="card-img-top"
                                                     src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                                                     alt="Card image cap">
                                             </div>
                                             <div class="col carousel_item_footer main_bg_color" style="color: white">
-
                                                 <div class="col float-left">
                                                     <p>{{$c->name}}<br>
                                                         {{$c->description}}</p>
@@ -998,7 +1024,37 @@
                                                     </p>
                                                 </div>
                                             </div>
+                                        </div> -->
+
+                                        <div class="col-md-4">
+                                          <div class="newcard">
+                                            <div class="card topsec">
+                                              <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
+                                                  alt="Card image cap">
+                                            </div>
+                                            <div class="col carousel_item_footer main_bg_color middlesec">
+                                                <div class="col float-left">
+                                                    <p class="middlesec-p1">{{$c->name}}<br>
+                                                      {{$c->description}}</p>
+                                                    <p class="middlesec-p2">{{$c->users->name}}, {{$c->users->experience}}</p>
+                                                    <p class="float-right p-0 mt-1 middlesec-p3">
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span> 5</span>
+                                                        <span>(12345)</span>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div class="bottomsec">
+                                              <img class="bottomsec-img" src="{{asset('assets/frontend/img/Mask Group 1.png')}}"  />
+                                              <p class="bottomsec-p">Mr Abcom</p>
+                                            </div>
+                                          </div>
                                         </div>
+
                                         @endforeach
 
                                     </div>
@@ -1014,7 +1070,7 @@
                                             @endphp
                                             @foreach($data1 as $c)
 
-                                            <div class="col-4 col-md-4 p-mob">
+                                            <!-- <div class="col-4 col-md-4 p-mob">
                                                 <div class="card">
                                                     <img class="card-img-top"
                                                         src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
@@ -1037,7 +1093,37 @@
                                                         </p>
                                                     </div>
                                                 </div>
+                                            </div> -->
+
+                                            <div class="col-md-4">
+                                          <div class="newcard">
+                                            <div class="card topsec">
+                                              <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
+                                                  alt="Card image cap">
                                             </div>
+                                            <div class="col carousel_item_footer main_bg_color middlesec">
+                                                <div class="col float-left">
+                                                    <p class="middlesec-p1">{{$c->name}}<br>
+                                                      {{$c->description}}</p>
+                                                    <p class="middlesec-p2">{{$c->users->name}}, {{$c->users->experience}}</p>
+                                                    <p class="float-right p-0 mt-1 middlesec-p3">
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span> 5</span>
+                                                        <span>(12345)</span>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div class="bottomsec">
+                                              <img class="bottomsec-img" src="{{asset('assets/frontend/img/Mask Group 1.png')}}"  />
+                                              <p class="bottomsec-p">Mr Abcom</p>
+                                            </div>
+                                          </div>
+                                        </div>
+
                                             @endforeach
 
                                         </div>
@@ -1048,14 +1134,17 @@
                         </div>
 
                         <div class="col-sm-1 controls-top">
-                            <a class="btn-floating" href="#multi-item-{{$cat->name}}" style="color: #570055" data-slide="next"><i
-                                    class="fa fa-chevron-right"></i></a>
+                            <a class="btn-floating" href="#multi-item-{{$cat->name}}" style="color: #570055" data-slide="next"><i class="fa fa-chevron-right"></i></a>
                         </div>
                         </div>
                         <!--/.Slides-->
 
                     </div>
                     <!--/.Carousel Wrapper-->
+
+                    <!-- <div class="slider_header text-center m-2 cat-title">
+                      <u style="text-decoration: none !important;">{{$cat->name}}</u>
+                    </div> -->
 
                     @endforeach
                 </div>
@@ -1066,18 +1155,13 @@
 
     <!-- slider close -->
 
-
-
-
-
-
     <!-- End -->
 
     <!--Description-->
-    <hr style="width: 100%; height: 5px; border-top:2px solid">
+    <hr class="mt-5" style="width: 100%; height: 5px; border-top:2px solid">
     <!-- Student Comment Section -->
-    <div class="heading mt-5">Student Feedback</div>
-    <div class="container mt-3 mb-3">
+    <div class="heading mt-4">Student Feedback</div>
+    <div class="container mt-5 mb-3">
         <div class="carousel slide multi-item-carousel" id="theCarousel">
             <div class="carousel-inner row mx-auto">
                 <div class="carousel-item active col-md-4 mb-5">
@@ -1151,40 +1235,46 @@
             </a>
         </div>
     </div>
+    <!-- <hr class="mt-2" style="width: 100%; height: 5px; border-top:2px solid"> -->
     <!-- End -->
     </div>
-    <!-- Books Reading Section -->
-    <div class="book-sec">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-sm-6 text-center">
-                    <h4><b>Become an instructor</b></h4>
-                    <p>Top instructors from around the world teach millions of students on Udemy. We provide the
-                        tools and skills to teach what you love.</p>
-                    <button class="btn plum-btn waves-effect waves-light">Start teaching today</button>
-                </div>
-                <div class="col-12 col-sm-6 text-center">
-                    <h4><b>Become an Student</b></h4>
-                    <p>Top instructors from around the world teach millions of students on Udemy. We provide the
-                        tools and skills to teach what you love.</p>
-                    <button class="btn plum-btn waves-effect waves-light">Start learning today</button>
-                </div>
+  </div>
+    
+    <div class="container-fluid px-0">
+      <!-- Books Reading Section -->
+      <div class="book-sec" style="padding: 50px 20px">
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-lg-5 col-md-12 col-sm-12 text-center">
+                <h4><b>Become an instructor</b></h4>
+                <p style="font-size: 14px;width: 80%;text-align: center;margin: auto;line-height: 24px">Top instructors from around the world teach millions of students on Udemy. We provide the
+                    tools and skills to teach what you love.</p>
+                <button class="btn plum-btn waves-effect waves-light mt-3">Start teaching today</button>
             </div>
-        </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 text-center">
+                <h4><b>Become an Student</b></h4>
+                <p style="font-size: 14px;width: 80%;text-align: center;margin: auto;line-height: 24px">Top instructors from around the world teach millions of students on Udemy. We provide the
+                    tools and skills to teach what you love.</p>
+                <button class="btn plum-btn waves-effect waves-light mt-3">Start learning today</button>
+            </div>
+            <div class="col-1"></div>
+          </div>
+      </div>
+      <!-- End -->
     </div>
-    <!-- End -->
     <div class="plum-bg">
         <div class="col-12 col-sm-10 offset-md-1" style="padding: 40px 15px;line-height: 1;">
             <div class="row">
-                <div class="col-12 col-sm-6">
+                <div class="col-lg-5 col-md-12 col-sm-12 my-2 text-center">
                     <!-- <div class="mb-1 white-text">Sign up to receive our weekly newsletter</div> -->
-                    <div class="white-text">Stay uploaded on all new online and offline training courses</div>
+                    <div class="white-text" style="padding-top: 10px">Stay uploaded on all new online and offline training courses</div>
                 </div>
-                <div class="col-12 col-sm-6">
+                <div class="col-lg-7 col-md-12 col-sm-12 my-2 text-center">
                     <div class="Subscription">
-                        <input class="SubInput z-depth-1" type="text" placeholder="Enter your email"><span
-                            class="SubBtn"><a
-                                href="#">Subscribe</a></span>
+                        <input class="SubInput z-depth-1" type="text" placeholder="Enter your email" style="width: 60%">
+                        <span class="SubBtn ml-2 px-4">
+                          <a href="#">Subscribe</a>
+                        </span>
                     </div>
                 </div>
             </div>
